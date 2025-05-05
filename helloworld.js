a53cfe6967c7b405ce92a641589a8f89da563f41
epcst-chua <RepeaterCreeper@gmail.com>
2025-05-05 09:33:23 +0800

    :zap: quiz

---
+++ helloworld.js
@@ -0,0 +1,177 @@
+
+let questionBank = {
+  questions: window.btoa(JSON.stringify([
+    {
+      header: "Evaluate the following expression:",
+      body: `<pre><code>true || false</code></pre>`,
+      type: 'boolean'
+    },
+    {
+      header: "Evaluate the following expression:",
+      body: `<pre><code>true && false</code></pre>`,
+      type: 'boolean'
+    },
+    {
+      header: "Evaluate the following expression:",
+      body: `<pre><code>!true</code></pre>`,
+      type: 'boolean'
+    },
+    {
+      header: "Evaluate the following expression:",
+      body: `<pre><code>!false</code></pre>`,
+      type: 'boolean'
+    },
+    {
+      header: "Evaluate the following expression:",
+      body: `<pre><code>!(true && false)</code></pre>`,
+      type: 'boolean'
+    },
+    {
+      header: "Evaluate the following expression:",
+      body: `<pre><code>!(true || false)</code></pre>`,
+      type: 'boolean'
+    },
+    {
+      header: "Evaluate the following expression:",
+      body: `<pre><code>!(true || true)</code></pre>`,
+      type: 'boolean'
+    },
+    {
+      header: "Evaluate the following expression:",
+      body: `<pre><code>true && !true</code></pre>`,
+      type: 'boolean'
+    },
+    {
+      header: "Evaluate the following expression:",
+      body: `<pre><code>true || !false</code></pre>`,
+      type: 'boolean'
+    },
+    {
+      header: "Determine the output of the following code:",
+      body: `<pre><code>
+                  let a = true;
+                  let b = false;
+                  console.log(a && b);    
+              </code></pre>`,
+      type: 'input'
+    },
+    {
+      header: "Determine the output of the following program:",
+      body: `<pre><code>
+                if(true) {
+                  console.log(true);
+                }
+                else {
+                  console.log(false);
+                }
+              </code></pre>`,
+      type: 'input'
+    },
+    {
+      header: "Determine the output of the following program:",
+      body: `<pre><code>
+                if(false) {
+                  console.log(true);
+                }
+                else {
+                  console.log(false);
+                }
+              </code></pre>`,
+      type: 'input'
+    },
+    {
+      header: "Determine the output of the following program:",
+      body: `<pre><code>
+                let a = false;
+                if(a) {
+                  console.log("Passed");
+                }
+                else {
+                  console.log("Failed");
+                }
+              </code></pre>`,
+      type: 'input'
+    },
+    {
+      header: "Determine the output of the following program:",
+      body: `<pre><code>
+                let a = false;
+                let b = false;
+                let c = a && b;
+                if(c) {
+                  console.log("I am Here");
+                }
+                else {
+                  console.log("Not There");
+                }
+              </code></pre>`,
+      type: 'input'
+    },
+    {
+      header: "What is this operator called?",
+      body: `||`,
+      type: 'input'
+    },
+    {
+      header: "What is this operator called?",
+      body: `&&`,
+      type: 'input'
+    },
+    {
+      header: "What is this operator called?",
+      body: `!`,
+      type: 'input'
+    },
+    {
+      header: "What is the final value of the `a` variable?",
+      body: `<pre><code>
+        let a = 5;
+        a = 3;
+        a + 1
+      </code></pre>`,
+      type: 'input'
+    },
+    {
+      header: "What is the final value of the `c` variable?",
+      body: `<pre><code>
+        let a = 5;
+        a = 3;
+        let c = a + 1;
+      </code></pre>`,
+      type: 'input'
+    },
+    {
+      header: "What is the final value of the `d` variable?",
+      body: `<pre><code>
+        let a = 5;
+        let d = a + 1;
+        let c = d + 2;
+      </code></pre>`,
+      type: 'input'
+    },
+    {
+      header: "Access the name property of the `a` variable",
+      body: `<pre><code>
+        let a = {
+          name: "Joseph Chua",
+          meta: {
+            grades: [32, 44, 98]
+          }
+        }
+      </code></pre>`,
+      type: 'input'
+    },
+    {
+      header: "Access the 3rd element of the `grades` property in the `a` variable",
+      body: `<pre><code>
+        let a = {
+          name: "Joseph Chua",
+          meta: {
+            grades: [32, 44, 98]
+          }
+        }
+      </code></pre>`,
+      type: 'input'
+    }
+  ]))
+};
